08dacf9fac8d014122e1753738858042864e9511
Chris Wong <wongislandd@gmail.com>
2025-01-15 12:04:10 -0500

    fixing edge to edge on iOS

--- composeApp/src/iosMain/kotlin/com/wongislandd/portfolio/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/wongislandd/portfolio/MainViewController.kt
@@ -1,5 +1,17 @@
 package com.wongislandd.portfolio
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.ComposeUIViewController
+import com.wongislandd.nexus.theming.AppTheme
 
-fun MainViewController() = ComposeUIViewController { PortfolioApp() }
+fun MainViewController() = ComposeUIViewController {
+    AppTheme {
+        PortfolioApp(
+            modifier = Modifier.background(MaterialTheme.colors.primary)
+                .safeDrawingPadding()
+        )
+    }
+}
